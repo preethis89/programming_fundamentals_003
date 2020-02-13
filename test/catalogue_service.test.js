abbd5e7145890c3310e9282c30b5e6531feee021
preethis89 <preethis89@outlook.com>
2020-02-13 14:33:02 +0000

    Exercise Complete

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,4 @@
 const catalogueService = require("../app/catalogue_service");
-
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
     test("returns the total number of books written by the given author", () => {
@@ -8,4 +7,32 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(catalogueService.checkBookByTitle("The Origin of Species")).toBe(true);
+    });
+  });
+  test("returns true if the book exists", () => {
+    expect(catalogueService.checkBookByTitle("The Chronicles of Narnia")).toBe(false);
+  });
 });
+describe("catalogueService.countBooksByFirstLetter", () => {
+  test("returns count of the books that start with that letter", () => {
+    expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+  });
+});
+describe("catalogueService.getQuantity", () => {
+  test(" returns quantity of the specfied book", () => {
+    expect(catalogueService.getQuantity("A Place of Greater Safety")).toBe(11);
+  });
+  test(" returns quantity of the specfied book", () => {
+    expect(catalogueService.getBooksByAuthor("Robert Bolaño")).toEqual([{ title: "2666", author: "Robert Bolaño", quantity: 17 },
+    { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }]);
+  });
+});
+  describe("catalogueService.checkQuantity", () => {
+    test("returns ttitle is in stock", () => {
+      expect(catalogueService.checkQuantity("By Night In Chile", 4)).toBe(true);
+      expect(catalogueService.checkQuantity("By Night In Chile", 100)).toBe(false);
+    });
+  });
